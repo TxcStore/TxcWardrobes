75529721a36b2d2163d30c29b5cc1249602be3a2
ToxicJellyTV <69016398+ToxicJellyTV@users.noreply.github.com>
2023-07-25 18:42:51 +0200

    fixed the export 'saveOutfit'
    The export 'saveOutfit' only saves the specified components, which are specified under Config.SavedComponents.

--- client/exports.lua
+++ client/exports.lua
@@ -1,4 +1,10 @@
-function saveOutfit(name, outfit)
+function saveOutfit(name, outfit, notify)
+    local outfitToSave = {}
+
+    for k, v in pairs(Config.SavedComponents) do
+        outfitToSave[v] = outfit[v]
+    end
+
     lib.callback('TxcWardrobes:SavePrivateOutfit', false, function(newOutfitList)
         --exports['TxcBase']:debugPrint(GetCurrentResourceName(), 'outfit saved successfully', 'success')
         savedOutfits = newOutfitList
